6e196ecc1cd48e3b33d4490d19dbe7b98c0d3be5
Seth <itz.saga@gmail.com>
2017-07-02 15:44:05 -0500

    Main component rendering. Removed Link for now.

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,6 @@ class App extends Component {
     return (
       <div>
         <Main />
-        <p>This is a paragraph with some <strong>bold text</strong> and some <em>italics text</em>.</p>
-        <a href="#">This is a link.</a>
-        <small>This is some small text.</small>
-        <sub>Subscript</sub>
-        <sup>Superscript</sup>
-        <code>Inline code</code>
-        <kbd>Keyboard Input</kbd>
-        <hr />
-        <pre>This is some preformatted text.</pre>
-        <blockquote cite="www.quotation.source">
-          This is some quoted text from another website or person.
-        </blockquote>
       </div>
     )
   }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const Main = () => {
-  return (
-    <div>
-      <h1>
-        <Link to='/'>Foodie View</Link>
-      </h1>
-    </div>
-  )
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Foodie View
+        </h1>
+      </div>
+    )
+  }
 }
 
 export default Main
