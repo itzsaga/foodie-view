60a9b2c0ca77599b12337e1b8d9b0132832b516e
Seth <itz.saga@gmail.com>
2017-07-02 16:02:13 -0500

    Added basic styling to Main title font.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import '../styles/main.css'
+
 class Main extends Component {
   render () {
     return (
       <div>
-        <h1>
+        <h1 className='title'>
           Foodie View
         </h1>
       </div>
