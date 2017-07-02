8e01b9cc4df634692508728a9ec79dfd4393c37a
Seth <itz.saga@gmail.com>
2017-07-02 15:39:53 -0500

    Started Main component.

---
+++ client/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Main = () => {
+  return (
+    <div>
+      <h1>
+        <Link to='/'>Foodie View</Link>
+      </h1>
+    </div>
+  )
+}
+
+export default Main
