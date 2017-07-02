08e92741f031ba2fd306cdd97492bafa33c9a420
Seth <itz.saga@gmail.com>
2017-07-02 15:39:38 -0500

    Started Main component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+// Components
+import Main from './components/Main'
+
 // CSS
 import 'mini.css'
 
@@ -7,6 +10,7 @@ class App extends Component {
   render () {
     return (
       <div>
+        <Main />
         <p>This is a paragraph with some <strong>bold text</strong> and some <em>italics text</em>.</p>
         <a href="#">This is a link.</a>
         <small>This is some small text.</small>
