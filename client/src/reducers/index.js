a09ebfde3f0abd33fc60f8b7b3897119ee51bca4
Seth <itz.saga@gmail.com>
2017-07-02 23:06:11 -0500

    Created rootReducer.

---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
+
+import comments from './comments'
+
+const rootReducer = combineReducers({
+  comments,
+  routing: routerReducer
+})
+
+export default rootReducer
