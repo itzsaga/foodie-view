03ecb6e64e6ba445ca8370413f3be68b6b975b61
Seth <itz.saga@gmail.com>
2017-07-02 20:18:13 -0500

    Empty List & Show components.

---
+++ client/src/components/YelpList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class YelpList extends Component {
+  render () {
+    return (
+    )
+  }
+}
+
+export default YelpList
---
+++ client/src/components/YelpShow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class YelpShow extends Component {
+  render () {
+    return (
+    )
+  }
+}
+
+export default YelpShow
