bc3e435fd45b20258f736b01bab8429f0e46f75c
Seth <itz.saga@gmail.com>
2017-07-03 16:09:03 -0500

    Basic styling for List.

--- client/src/components/RestaurantList.js
+++ client/src/components/RestaurantList.js
@@ -4,6 +4,8 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as actionCreators from '../actions/actionCreators'
 
+import '../styles/restaurantList.css'
+
 import Restaurant from './Restaurant'
 
 class RestaurantList extends Component {
