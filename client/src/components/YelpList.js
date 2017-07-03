c443d7204e86940a3da534bf9d1b3c1d35b4cc64
Seth <itz.saga@gmail.com>
2017-07-02 20:20:11 -0500

    Mocked List & Show.

--- client/src/components/YelpList.js
+++ client/src/components/YelpList.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 class YelpList extends Component {
   render () {
     return (
+      <div className="yelp-list">
+        I'm the Yelp List.
+      </div>
     )
   }
 }
--- client/src/components/YelpShow.js
+++ client/src/components/YelpShow.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 class YelpShow extends Component {
   render () {
     return (
+      <div className="yelp-show">
+        I'm the Yelp Show.
+      </div>
     )
   }
 }
