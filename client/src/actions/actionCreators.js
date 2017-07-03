2ad9cc104b89e82b19d969739063e3825db448ef
Seth <itz.saga@gmail.com>
2017-07-02 22:37:39 -0500

    Initial actions done.

--- client/src/actions/actionCreators.js
+++ client/src/actions/actionCreators.js
@@ -1,5 +1,26 @@
 // increment
+function increment (itemId) {
+  return {
+    type: 'INCREMENT_LIKES',
+    itemId
+  }
+}
 
 // add comment
+function addComment (itemId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    itemId,
+    author,
+    comment
+  }
+}
 
 // remove comment
+function removeComment (itemId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    itemId,
+    i
+  }
+}
