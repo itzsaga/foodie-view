e810d04d302c041085272e4dd194237af8fe2ccc
Seth <itz.saga@gmail.com>
2017-07-03 21:35:35 -0500

    ADD_COMMENT working.

--- client/src/reducers/comments.js
+++ client/src/reducers/comments.js
@@ -1,5 +1,23 @@
+function restaurantComments (state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT':
+      return state
+    default:
+      return state
+  }
+}
 function comments (state = [], action) {
-  console.log(state, action)
+  if (typeof action.id !== 'undefined') {
+    return {
+      ...state,
+      [action.id]: restaurantComments(state[action.id], action)
+    }
+  }
   return state
 }
 
