170e4b755b6dd7ec6937cfb5b4884a95f7b66294
Seth <itz.saga@gmail.com>
2017-07-03 20:32:44 -0500

    Refactored Restaurant to functional stateless component.

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class Restaurant extends Component {
-  render () {
-    const { restaurant, comments } = this.props
-    return (
-      <figure className='grid-figure'>
-        <div className='grid-photo-wrap'>
-          <Link to={`/restaurants/${restaurant.id}`}>
-            <img src={`${restaurant.image_url}`} alt={restaurant.name} className='grid-photo' />
+// class Restaurant extends Component {
+//   render () {
+export default (props) => {
+  const { restaurant, comments } = props
+  return (
+    <figure className='grid-figure'>
+      <div className='grid-photo-wrap'>
+        <Link to={`/restaurants/${restaurant.id}`}>
+          <img src={`${restaurant.image_url}`} alt={restaurant.name} className='grid-photo' />
+        </Link>
+      </div>
+
+      <figcaption>
+        <p>{restaurant.name}</p>
+        <div className='control-buttons'>
+          <Link className='button' to={`/restaurants/${restaurant.id}`}>
+            <span className='comment-count'>
+              <span className='speech-bubble' />
+              <span> {comments[restaurant.id] ? comments[restaurant.id].length : 0 }</span>
+            </span>
           </Link>
         </div>
-
-        <figcaption>
-          <p>{restaurant.name}</p>
-          <div className='control-buttons'>
-            <Link className='button' to={`/restaurants/${restaurant.id}`}>
-              <span className='comment-count'>
-                <span className='speech-bubble' />
-                <span> {comments[restaurant.id] ? comments[restaurant.id].length : 0 }</span>
-              </span>
-            </Link>
-          </div>
-        </figcaption>
-      </figure>
-    )
-  }
+      </figcaption>
+    </figure>
+  )
 }
+// }
 
-export default Restaurant
+// export default Restaurant
