d2b8277f9f08d981ed55d10d9447298c8c06a5e2
Seth <itz.saga@gmail.com>
2017-07-03 23:45:29 -0500

    DB seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(place_id: 'painturos-lebanon',
+                  name: "Painturo's",
+                  image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/J8KrToIKl_DvWk8AwmE3oA/o.jpg')
+Place.create(place_id: 'prince-street-pizza-and-pub-gallatin',
+                  name: 'Prince Street Pizza and Pub',
+                  image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/DJEH-VZDnkxPAXV6lH5aEA/o.jpg')
+Place.create(place_id: 'calabria-brickoven-pizzeria-mount-juliet',
+                  name: 'Calabria Brickoven Pizzeria',
+                  image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/6BWVGS-dYVlNIfxlwqe7vQ/o.jpg')
+Place.create(place_id: 'nona-lisa-pizzeria-watertown',
+                  name: 'Nona Lisa Pizzeria',
+                  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/C0XtKNtge3IW3TNH_AGxQA/o.jpg')
+Place.create(place_id: 'diciccos-pizzeria-lebanon',
+                  name: "DiCicco's Pizzeria",
+                  image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/EZRZJwcMssD0QkOFwAC0Xw/o.jpg')
+Place.create(place_id: 'five-points-pizza-nashville',
+                  name: 'Five Points Pizza',
+                  image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/aJfhk8ebIWuw599rvKByow/o.jpg')
+Place.create(place_id: 'kos-pizza-and-cafe-lebanon',
+                  name: "Ko's Pizza & Cafe",
+                  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/3bBHmN1GoA3fHVyHF1NSaw/o.jpg')
+Place.create(place_id: 'daplace_id-pizza-and-restaurant-lebanon',
+                  name: "Daplace_ids Pizza",
+                  image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/o8sKyqxFvGpTg7DbU2uc5Q/o.jpg')
+Place.create(place_id: 'campiones-taste-of-chicago-gallatin',
+                  name: "Campione's Taste of Chicago",
+                  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/oVYFjJisWgkMMjCY4Q4qNw/o.jpg')
+Place.create(place_id: 'desano-pizza-bakery-nashville',
+                  name: 'DeSano Pizza Bakery',
+                  image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/p65M38H7Cr38eQsgudU1Mg/o.jpg')
+Place.create(place_id: 'sammy-bs-lebanon',
+                  name: "Sammy B's",
+                  image_url: 'https://s3-media2.fl.yelpcdn.com/bphoto/NoHcDYGDTI1o7oj3fWBQWQ/o.jpg')
+Place.create(place_id: 'ny-pie-hendersonville-2',
+                  name: 'NY Pie',
+                  image_url: 'https://s3-media4.fl.yelpcdn.com/bphoto/1wFFNXjw9ne-PF0bw77fsA/o.jpg')
+Place.create(place_id: 'joeys-house-of-pizza-nashville',
+                  name: "Joey's House Of Pizza",
+                  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/yd0x9iHHpNO9Y2_lJrqvtw/o.jpg')
+Place.create(place_id: 'slim-and-huskys-pizza-beeria-nashville',
+                  name: "Slim & Husky's Pizza Beeria",
+                  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/t-DZIrS99xkJJtUrCne_Vg/o.jpg')
