31fce3f7237ad4ff178b758a701ba648ed4f56da
Seth <itz.saga@gmail.com>
2017-07-03 21:05:30 -0500

    Rendering comments on Comments component.

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 class Comments extends React.Component {
+  renderComment (comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  }
   render () {
     return (
-      <div className='comment'>
-        I'm a comment.
+      <div className='comments'>
+        {this.props.restaurantComments.map(this.renderComment)}
       </div>
     )
   }
