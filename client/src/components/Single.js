fab2d53e010131d0984fafd3e1d411d03ea5a8e6
Seth <itz.saga@gmail.com>
2017-07-03 20:49:25 -0500

    Started work on Single component.

--- client/src/components/Single.js
+++ client/src/components/Single.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as actionCreators from '../actions/actionCreators'
+import Restaurant from './Restaurant'
 
-export default class Single extends Component {
+class Single extends Component {
   render () {
+    const i = this.props.restaurants.businesses.findIndex((restaurant) => restaurant.id === this.props.match.params.id)
+    const restaurant = this.props.restaurants.businesses[i]
+    console.log(restaurant)
     return (
       <div className='single'>
-        <p>I'm the Show.</p>
+        <Restaurant i={i} restaurant={restaurant} {...this.props} />
       </div>
     )
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    restaurants: state.restaurants,
+    comments: state.comments
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(actionCreators, dispatch)
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Single))
