e321c125462704dfe3febec71cc72f88d7c3e876
Seth <itz.saga@gmail.com>
2017-07-06 14:04:31 -0500

    Added routes to handle creating new places.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api' do
-    get :places, to: 'places#index'
+    resources :places, only: %w[index create new]
+    # get :places, to: 'places#index'
   end
 end
