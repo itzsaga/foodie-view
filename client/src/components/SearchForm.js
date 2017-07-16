fe9ba4042e2803ceafb53e6097174570b2f71174
Seth <itz.saga@gmail.com>
2017-07-16 15:38:03 -0500

    Added state to SearchForm to handle form changes.

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -2,5 +2,12 @@ import React, { Component } from 'react'
 import * as actionCreators from '../actions/actionCreators'
 
 export default class SearchForm extends Component {
+  constructor () {
+    super()
 
+    this.state = {
+      name: '',
+      zip_code: ''
+    }
+  }
 }
