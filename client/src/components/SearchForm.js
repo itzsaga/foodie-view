1bf11922032dda3b4b6dac2366e006e9cbfbf0fc
Seth <itz.saga@gmail.com>
2017-07-16 15:46:16 -0500

    Added form to SearchForm.

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -10,4 +10,37 @@ export default class SearchForm extends Component {
       zip_code: ''
     }
   }
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label>
+            Name:
+          </label>
+          <input id='restaurant-name'
+            type='text'
+            name='name'
+            value={this.state.name}
+            onChange={this.handleChange}
+            required />
+        </div>
+        <div>
+          <label>
+            Zip Code:
+          </label>
+          <input id='restaurant-zip_code'
+            type='number'
+            name='zip_code'
+            value={this.state.zip_code}
+            onChange={this.handleChange}
+            pattern='[0-9]{5}'
+            required />
+        </div>
+        <div>
+          <button type='submit'>Search</button>
+        </div>
+      </form>
+    )
+  }
 }
